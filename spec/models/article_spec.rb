c21043501bb5bfcda955ab56d57e0ef36138ff58
今永翔太 <>
2021-01-01 16:30:07 +0900

    article_spec.rb を修正

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -13,7 +13,7 @@
 #
 #  index_articles_on_user_id  (user_id)
 #
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Article, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
