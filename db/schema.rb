9e9ba72c9369814e89f5fd26940a4fda2858d241
今永翔太 <>
2021-01-01 16:34:43 +0900

    article_like モデルを作成してmigrationを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_01_065252) do
+ActiveRecord::Schema.define(version: 2021_01_01_073223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "article_likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_article_likes_on_article_id"
+    t.index ["user_id"], name: "index_article_likes_on_user_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -48,4 +57,6 @@ ActiveRecord::Schema.define(version: 2021_01_01_065252) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "article_likes", "articles"
+  add_foreign_key "article_likes", "users"
 end
