18a744a5b1aae85a28d861bdbc1415921dc1cdef
今永翔太 <>
2021-01-03 14:00:59 +0900

    rubocopによる指摘箇所の修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,14 +3,14 @@ require "rails_helper"
 RSpec.describe User, type: :model do
   context "name が指定しているとき" do
     it "ユーザーが作られる" do
-      user = User.new(name:"foo", email:"foo@example.com", password: "foo123456")
+      user = User.new(name: "foo", email: "foo@example.com", password: "foo123456")
       expect(user).to be_valid
     end
   end
 
   context "name が指定していないとき" do
     it "ユーザー作成が失敗する" do
-      user = User.new(email:"foo@example.com", password: "foo123456")
+      user = User.new(email: "foo@example.com", password: "foo123456")
       expect(user).to be_invalid
     end
   end
@@ -21,7 +21,7 @@ RSpec.describe User, type: :model do
   end
 
   context "すでに同じ名前の name が存在しているとき" do
-    it "ユーザー作成に失敗する"do
+    it "ユーザー作成に失敗する" do
     end
   end
 end
