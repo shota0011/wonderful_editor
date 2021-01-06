184cdcc6030e7f2c2f97afecb85aad051f2dcc69
今永翔太 <>
2021-01-06 23:26:35 +0900

    rubocop による指摘箇所の修正

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -16,23 +16,22 @@
 require "rails_helper"
 
 RSpec.describe Article, type: :model do
-
   context "タイトルと本文が入力されているとき" do
     let(:article) { build(:article) }
-      it "記事の作成が出来る" do
-        expect(article).to be_valid
-      end
+    it "記事の作成が出来る" do
+      expect(article).to be_valid
+    end
   end
 
   context "タイトルが未記入" do
-    let(:article) { build(:article, title:nil) }
+    let(:article) { build(:article, title: nil) }
     it "エラーが発生" do
       expect(article).to be_invalid
     end
   end
 
-  fcontext "本文が未記入" do
-    let(:article) { build(:article, body:nil) }
+  context "本文が未記入" do
+    let(:article) { build(:article, body: nil) }
     it "エラーが発生" do
       expect(article).to be_valid
     end
