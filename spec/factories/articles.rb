1de63a04c98bb06195dff5ccd3ec2d9f4fa62bc3
今永翔太 <>
2021-01-06 23:19:47 +0900

    article モデルにテストを記入

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -15,8 +15,8 @@
 #
 FactoryBot.define do
   factory :article do
-    title { "MyString" }
-    body { "MyText" }
-    user { "" }
+    title { "Faker::Lorem.word" }
+    body { "Faker::Lorem.sentence" }
+    user
   end
 end
