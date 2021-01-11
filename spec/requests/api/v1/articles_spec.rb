bd8e493ea9be26e7d3bca2e6274dfc8acc2273aa
今永翔太 <>
2021-01-11 16:57:38 +0900

    article_spec の修正

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -3,17 +3,12 @@ require "rails_helper"
 RSpec.describe "Api::V1::Articles", type: :request do
   describe "GET /articles" do
     subject { get(api_v1_articles_path) }
-<<<<<<< HEAD
 
     let(:article) { create(:article) }
 
     it "記事一覧を取得する" do
-=======
-    let(:article) { create(:article)
-    fit "returns http success" do
->>>>>>> b6369dbb3d8bd6bc8061df430e4d2e8686cc16fa
       subject
-      
+
       # get "/api/v1/articles/index"
       # expect(response).to have_http_status(:success)
     end
