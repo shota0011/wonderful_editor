a8da3b190057d31e324ba8208254dca4fffd3e42
今永翔太 <>
2021-01-11 16:01:16 +0900

    rubocop による指摘箇所の修正

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -1,10 +1,10 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Api::V1::Articles", type: :request do
   describe "GET /articles" do
     subject { get(api_v1_articles_path) }
     let(:article) { create(:article) }
-    fit "returns http success" do
+    it "returns http success" do
       subject
       # get "/api/v1/articles/index"
       # expect(response).to have_http_status(:success)
