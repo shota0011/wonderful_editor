eadecff59dcd1461220e1400774e1418eaeb0bb8
今永翔太 <>
2021-01-11 15:46:12 +0900

    article_spec の修正

---
+++ spec/requests/api/v1/articles_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::Articles", type: :request do
+  describe "GET /articles" do
+    subject { get(api_v1_articles_path) }
+    let(:article) { create(:article) }
+    fit "returns http success" do
+      subject
+      # get "/api/v1/articles/index"
+      # expect(response).to have_http_status(:success)
+    end
+  end
+end
