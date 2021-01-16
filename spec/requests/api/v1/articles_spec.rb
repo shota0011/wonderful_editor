5be64e781f0d741881cdeb1ab1d58c772e59aeb7
今永翔太 <>
2021-01-16 09:38:04 +0900

    article_spec の修正

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -1,18 +1,20 @@
 require "rails_helper"
 
 RSpec.describe "Api::V1::Articles", type: :request do
-  fdescribe "GET /articles" do
+  describe "GET /articles" do
     subject { get(api_v1_articles_path) }
     before { create_list(:article, 3) }
 
     #記事一覧を更新順に並べる
     # let(:article) { create(:article, :updated_at) }
-    it "記事一覧を取得する" do
+    fit "記事一覧を取得する" do
       subject
+      res = JSON.parse(response.body)
       #このテスト(expect)で何を確認すべきか
       #記事の一覧が更新順にかえって来ることを確認する
+      expect(response).to have_http_status(:ok)
+      expect(res[0].keys).to eq ["id", "title", "updated_at", "user"]
       binding.pry
-      # expect(response).to have_http_status(:success)
     end
   end
 end
