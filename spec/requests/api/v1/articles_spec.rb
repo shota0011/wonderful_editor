00201b7129afa14a1bb80ee90e2ec4519e6d672d
今永翔太 <>
2021-01-17 09:40:36 +0900

    articles_spec.rb の修正

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -1,18 +1,40 @@
 require "rails_helper"
 
 RSpec.describe "Api::V1::Articles", type: :request do
-  fdescribe "GET /articles" do
+  describe "GET /articles" do
     subject { get(api_v1_articles_path) }
     before { create_list(:article, 3) }
-
-    #記事一覧を更新順に並べる
-    # let(:article) { create(:article, :updated_at) }
     it "記事一覧を取得する" do
       subject
-      #このテスト(expect)で何を確認すべきか
-      #記事の一覧が更新順にかえって来ることを確認する
-      binding.pry
-      # expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+
+  describe "GET /articles/:id" do
+    subject { get (api_v1_article_path(article_id)) }
+    #正常系
+    context "指定した id のユーザーが存在するとき" do
+      # let!(:article){ get(api_v1_articles_path(:article_id)) }
+      # let!(:article){ create_list(:article, 3) }
+      let(:article) { create(:article)}
+      let(:article_id){ article.id }
+
+      fit "対象の id 記事を取得する" do
+        subject
+        res = JSON.parse(response.body)
+        expect(res[:id]).to eq article.id
+        binding.pry
+      
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    #異常系
+    context "指定した id の記事が見つからない" do
+      it "ユーザーが見つからない" do
+
+      end
     end
   end
 end
