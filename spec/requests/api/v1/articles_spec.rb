7e36ef135e1d3dcf99a46b78f197250c882c8f72
今永翔太 <>
2021-01-18 12:49:49 +0900

    rubocop による指摘箇所の修正

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -3,22 +3,24 @@ require "rails_helper"
 RSpec.describe "Api::V1::Articles", type: :request do
   describe "GET /articles" do
     subject { get(api_v1_articles_path) }
+
     before { create_list(:article, 3) }
+
     it "記事一覧を取得する" do
       subject
       expect(response).to have_http_status(:ok)
     end
   end
 
-
   describe "GET /articles/:id" do
-    subject { get (api_v1_article_path(article_id)) }
+    subject { get(api_v1_article_path(article_id)) }
     # 正常系
-    context "指定した id のユーザーが存在するとき" do
-      let(:article) { create(:article)}
-      let(:article_id){ article.id }
 
-      it "対象のユーザー id の記事を取得する" do
+    context "指定した id のユーザーが存在するとき" do
+      let(:article) { create(:article) }
+      let(:article_id) { article.id }
+
+      it "任意の記事の値が取得出来る" do
         subject
         res = JSON.parse(response.body)
         expect(res["id"]).to eq article.id
@@ -30,11 +32,11 @@ RSpec.describe "Api::V1::Articles", type: :request do
       end
     end
 
-    #異常系
+    # 異常系
     context "指定した id の記事が見つからない" do
-      let(:article_id)  { 999999 }
-      fit "ユーザー id が見つからない" do
-        expect{ subject }.to raise_error(ActiveRecord::RecordNotFound)
+      let(:article_id) { 999999 }
+      it "ユーザー id が見つからない" do
+        expect { subject }.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
   end
