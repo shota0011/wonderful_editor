e394e05b0059baec4a69e5d70222c63738619f55
今永翔太 <>
2021-02-07 09:42:32 +0900

    新規登録のテスト実装・正常系完了

--- spec/requests/api/v1/auth/registrations_request_spec.rb
+++ spec/requests/api/v1/auth/registrations_request_spec.rb
@@ -6,32 +6,40 @@ RSpec.describe "Api/V1::Auth::Registrations", type: :request do
     #正常系
     context "ユーザーの情報がすべて入力されている" do
       let(:params) { attributes_for(:user) }
-      fit "新規登録が出来る" do
-        binding.pry
+      it "新規登録が出来る" do
         expect{ subject }.to change { User.count }.by(1)
+        expect(response).to have_http_status(:ok)
+      end
+
+      fit "ヘッダーの情報が取得出来ている" do
+        subject
+        header = response.header
         binding.pry
+        subject
+        expect(header["access-token"]).to be_present
+        expect(header["token-type"]).to be_present
+        expect(header["client"]).to be_present
+        expect(header["expiry"]).to be_present
+        expect(header["uid"]).to be_present
       end
     end
-    #ヘッダー情報が取得できているかどうか
-    contxet "" do
+
+    #異常系
+    context "nameが存在しないとき" do
+      let(:params) { name :nil }
+      # res = JSON.parse(response.body)
+      it "エラーになる" do
+        binding.pry
+        subject
+        # expect { subject }.to raise_error ()
+      end
+    end
+
+    context "emailが存在しないとき" do
+
+    end
+    context "passwordが存在しないとき" do
 
     end
   end
-    #異常系
-  #   context "nameが存在しないとき" do
-  #     let(:params) { name :nil }
-  #     # res = JSON.parse(response.body)
-  #     it "エラーになる" do
-  #       binding.pry
-  #       subject
-  #       # expect { subject }.to raise_error ()
-  #     end
-  #   end
-
-  #   context "emailが存在しないとき" do
-
-  #   end
-  #   context "passwordが存在しないとき" do
-
-  #   end
 end
