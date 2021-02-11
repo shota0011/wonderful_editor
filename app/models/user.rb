04aedc073dbeaf0f95d8eac7078d5209fc2c909d
今永翔太 <>
2021-02-11 22:17:55 +0900

    postgresqlからmysqlへ変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,19 +7,19 @@
 #  id                     :bigint           not null, primary key
 #  allow_password_change  :boolean          default(FALSE)
 #  confirmation_sent_at   :datetime
-#  confirmation_token     :string
+#  confirmation_token     :string(255)
 #  confirmed_at           :datetime
-#  email                  :string
-#  encrypted_password     :string           default(""), not null
-#  image                  :string
-#  name                   :string
-#  provider               :string           default("email"), not null
+#  email                  :string(255)
+#  encrypted_password     :string(255)      default(""), not null
+#  image                  :string(255)
+#  name                   :string(255)
+#  provider               :string(255)      default("email"), not null
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
-#  reset_password_token   :string
+#  reset_password_token   :string(255)
 #  tokens                 :json
-#  uid                    :string           default(""), not null
-#  unconfirmed_email      :string
+#  uid                    :string(255)      default(""), not null
+#  unconfirmed_email      :string(255)
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,7 +3,7 @@
 # Table name: comments
 #
 #  id         :bigint           not null, primary key
-#  body       :text
+#  body       :text(65535)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  article_id :bigint           not null
