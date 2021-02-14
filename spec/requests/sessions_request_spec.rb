e6d6e358691e49d7450fa4c27c093fb008182b9b
今永翔太 <>
2021-02-14 13:04:55 +0900

    ログインのテスト実装

--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -1,10 +1,21 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "api/v1::auth::Sessions", type: :request do
   describe "name,email,passwordのすべてが入力されている" do
-    subject { POST }
-    it "ログイン出来る" do
+    subject { post(api_v1_user_session_path, params: params) }
 
+    let(:user) { create(:user) }
+    let(:params) { attributes_for(:user, email: user.email, password: user.password) }
+
+    context "name,email,passwordのすべてが入力されている" do
+      it "ログイン出来る" do
+        subject
+        header = response.header
+        expect(header["access-token"]).to be_present
+        expect(header["client"]).to be_present
+        expect(header["uid"]).to be_present
+        expect(response).to have_http_status(:ok)
+      end
     end
   end
 end
