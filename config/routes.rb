6b457122424aa3ef5247b51785171afbd0c57b0a
今永翔太 <>
2021-02-27 23:14:52 +0900

    下書きの一覧と詳細のAPIを実装

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
       mount_devise_token_auth_for "User", at: "auth", controllers: {
         registrations: "api/v1/auth/registrations",
       }
+      namespace :article do
+        resources :draft, only: [:index, :show]
+      end
       resources :articles
     end
   end
