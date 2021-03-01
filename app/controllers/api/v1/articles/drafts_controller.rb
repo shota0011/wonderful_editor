533e83266c526700e3747c8ecaa61ac327472a75
今永翔太 <>
2021-03-01 20:12:14 +0900

    下書きのテスト実装とディレクトリの階層を修正

--- app/controllers/api/v1/articles/drafts_controller.rb
+++ app/controllers/api/v1/articles/drafts_controller.rb
@@ -1,5 +1,6 @@
 module Api::V1
   class Articles::DraftsController < BaseApiController
+    before_action :authenticate_user!
 
     def index
       articles = current_user.articles.draft.order(updated_at: :desc)
