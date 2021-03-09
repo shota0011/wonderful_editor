2d5cd88afb18a8150011cc60aee03f729c6ff277
今永翔太 <>
2021-03-09 10:04:22 +0900

    rubocopによる指摘箇所の修正

--- spec/requests/api/v1/articles/drafts_spec.rb
+++ spec/requests/api/v1/articles/drafts_spec.rb
@@ -1,5 +1,5 @@
 require "rails_helper"
-#テストの概要
+# テストの概要
 #  -自分が書いた記事の下書き一覧を取得する
 #    -記事一覧が取得出来る
 #
@@ -9,7 +9,6 @@ require "rails_helper"
 
 RSpec.describe "Api::V1::Articles::Drafts", type: :request do
   describe "GET /api/v1/articles/drafts" do
-
     subject { get(api_v1_articles_drafts_path, headers: headers) }
 
     context "自分が書いた記事の下書き一覧を取得する" do
